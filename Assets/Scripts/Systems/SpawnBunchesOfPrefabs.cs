29b247b65aa1ac78b2f59cd55ecd1cfe8989d967
FireFlyForLife <maiko.s.99@gmail.com>
2020-02-23 12:15:36 +0100

    Added spawning

--- Assets/Scripts/Systems/SpawnBunchesOfPrefabs.cs
+++ Assets/Scripts/Systems/SpawnBunchesOfPrefabs.cs
@@ -16,119 +16,58 @@ public struct SpawnerOfBunches : IComponentData
     public bool hasSpawned;
 }
 
-public class SpawnBunchesOfPrefabsSyncronously : ComponentSystem
+
+public class SpawnBunchesOfPrefabs : JobComponentSystem
 {
-    protected override void OnUpdate()
-    {
-        Entities.ForEach((ref SpawnerOfBunches spawner, ref LocalToWorld localToWorld) => SpawnPrefabs(ref spawner, ref localToWorld) );
-    }
+    // Not a long name at all
+    private EndSimulationEntityCommandBufferSystem endSimulationEntityCommandBufferSystem;
 
-    private void SpawnPrefabs(ref SpawnerOfBunches spawner, ref LocalToWorld localToWorld)
+    //[BurstCompile]
+    struct SpawnBunchesOfPrefabsJob : IJobForEachWithEntity<SpawnerOfBunches, LocalToWorld>
     {
-        if (spawner.hasSpawned)
-            return;
-        spawner.hasSpawned = true;
+        public EntityCommandBuffer.Concurrent entityCommandBuffer;
 
-        for (int z = 0; z < spawner.amounts.z; z++)
+        public void Execute(Entity entity, int index, ref SpawnerOfBunches spawner, [ReadOnly] ref LocalToWorld localToWorld)
         {
-            for (int y = 0; y < spawner.amounts.y; y++)
-            {
-                for (int x = 0; x < spawner.amounts.x; x++)
-                {
-                    Entity instance = World.EntityManager.Instantiate(spawner.prefab);
+            if (spawner.hasSpawned)
+                return;
+            spawner.hasSpawned = true;
 
-                    float4x4 parentTransform =
-                        localToWorld.Value * float4x4.Translate(new float3(x, y, z) * spawner.padding);
-                    RecursivelyTransform(instance, ref parentTransform);
+            for (int z = 0; z < spawner.amounts.z; z++)
+            {
+                for (int y = 0; y < spawner.amounts.y; y++)
+                {
+                    for (int x = 0; x < spawner.amounts.x; x++)
+                    {
+                        Entity instance = entityCommandBuffer.Instantiate(index, spawner.prefab);
+                        entityCommandBuffer.SetComponent(index, instance, new Translation
+                        {
+                            Value = localToWorld.Position + new float3(x, y, z) * spawner.padding
+                        });
+
+                    }
                 }
             }
         }
     }
 
-    private void RecursivelyTransform(Entity parent, ref float4x4 parentTransform)
+    protected override void OnCreate()
     {
-        World.EntityManager.SetComponentData(parent, new LocalToWorld
-        {
-            Value = parentTransform
-        });
+        endSimulationEntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
 
-        if (World.EntityManager.HasComponent<LinkedEntityGroup>(parent))
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new SpawnBunchesOfPrefabsJob
         {
-            var children = World.EntityManager.GetBuffer<LinkedEntityGroup>(parent);
+            entityCommandBuffer = endSimulationEntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent()
+        };
 
-            foreach (var child in children)
-            {
-                LocalToWorld childLocalToWorld = World.EntityManager.GetComponentData<LocalToWorld>(child.Value);
-                float4x4 localToWorld = parentTransform * childLocalToWorld.Value;
-                RecursivelyTransform(child.Value, ref localToWorld);
-            }
-        }
+        // Now that the job is set up, schedule it to be run. 
+        JobHandle jobHandle = job.Schedule(this, inputDependencies);
+
+        endSimulationEntityCommandBufferSystem.AddJobHandleForProducer(jobHandle);
+
+        return jobHandle;
     }
 }
-
-//public class SpawnBunchesOfPrefabs : JobComponentSystem
-//{
-//    // Not a long name at all
-//    private EndSimulationEntityCommandBufferSystem endSimulationEntityCommandBufferSystem;
-
-//    //[BurstCompile]
-//    struct SpawnBunchesOfPrefabsJob : IJobForEachWithEntity<SpawnerOfBunches, LocalToWorld>
-//    {
-//        public EntityCommandBuffer.Concurrent entityCommandBuffer;
-
-//        public void Execute(Entity entity, int index, ref SpawnerOfBunches spawner, [ReadOnly] ref LocalToWorld localToWorld)
-//        {
-//            if (spawner.hasSpawned)
-//                return;
-//            spawner.hasSpawned = true;
-
-//            for (int z = 0; z < spawner.amounts.z; z++)
-//            {
-//                for (int y = 0; y < spawner.amounts.y; y++)
-//                {
-//                    for (int x = 0; x < spawner.amounts.x; x++)
-//                    {
-//                        Entity instance = entityCommandBuffer.Instantiate(index, spawner.prefab);
-//                        entityCommandBuffer.SetComponent(index, instance, new Translation
-//                        {
-//                            Value = localToWorld.Position + new float3(x, y, z) * spawner.padding
-//                        });
-                        
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-//    protected override void OnCreate()
-//    {
-//        endSimulationEntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-//    }
-
-//    protected override JobHandle OnUpdate(JobHandle inputDependencies)
-//    {
-//        EntityQuery query = GetEntityQuery(typeof(SpawnerOfBunches), ComponentType.ReadOnly<LocalToWorld>());
-
-//        var spawners = query.ToComponentDataArray<SpawnerOfBunches>(Allocator.Temp);
-//        foreach (var spawner in spawners)
-//        {
-//            var children = World.EntityManager.GetBuffer<LinkedEntityGroup>(spawner.prefab);
-//            foreach (var child in children)
-//            {
-                
-//            }
-//        }
-        
-//        var job = new SpawnBunchesOfPrefabsJob
-//        {
-//            entityCommandBuffer = endSimulationEntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent()
-//        };
-        
-//        // Now that the job is set up, schedule it to be run. 
-//        JobHandle jobHandle = job.Schedule(this, inputDependencies);
-
-//        endSimulationEntityCommandBufferSystem.AddJobHandleForProducer(jobHandle);
-
-//        return jobHandle;
-//    }
-//}
